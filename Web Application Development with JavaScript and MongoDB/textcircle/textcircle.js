ca6d88db3fecefd13587ccbd6e3f318a214c2239
taylorty <taylor4869@gmail.com>
2015-12-17 12:16:47 -0500

    Initial commit

---
+++ Web Application Development with JavaScript and MongoDB/textcircle/textcircle.js
@@ -0,0 +1,21 @@
+this.Documents = new Mongo.Collection("documents");
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Template.editor.helpers({
+    docid:function(){
+      return Documents.findOne()._id;
+    }
+
+  })
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    if (!Documents.findOne()){ //no documents
+      Documents.insert({title:"my new document"});
+
+    }
+  });
+}
